22074a3fc179a8d3198adfdfb7023e324e0160a5
PiyushYadav88 <110554128+PiyushYadav88@users.noreply.github.com>
2022-09-22 15:12:39 +0530

    grocery app full code

---
+++ app/src/main/java/com/piyush/groceryapp/GroceryItems.kt
@@ -0,0 +1,22 @@
+package com.piyush.groceryapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "Grocery_items")
+data class GroceryItems (
+    @ColumnInfo(name = "itemName")
+    var itemName:String,
+
+    @ColumnInfo(name = "itemQuantity")
+    var itemQuantity:Double,
+
+    @ColumnInfo(name = "itemPrice")
+    var itemPrice:Double,
+)
+{
+    @PrimaryKey(autoGenerate = true)
+    var id:Int?=null
+}
